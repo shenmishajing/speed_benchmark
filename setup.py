58b886647004ee69bce455ca0de21b0a624a8028
shenmishajing <shenmishajing@gmail.com>
2023-06-30 13:02:09 +0800

    fix import bug

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="speed-benchmark",
-    version="1.0.0",
+    version="1.0.1",
     description="A generic speed benchmark library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- speed_benchmark/__init__.py
+++ speed_benchmark/__init__.py
@@ -1,2 +1,3 @@
-from .speed_benchmark import *
-from .visualization import *
+from .speed_benchmark import speed_benchmark as speed_benchmark
+from .visualization import draw_line_chart as draw_line_chart
+from .visualization import ncolors as ncolors
