3dc7767ac092fcfcb7a44707ef4f23f77f309ef6
shenmishajing <shenmishajing@gmail.com>
2023-07-02 22:59:55 +0800

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="speed-benchmark",
-    version="1.0.2",
+    version="1.0.3",
     description="A generic speed benchmark library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
