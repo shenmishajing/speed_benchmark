298fdb353aa6cd434cb196f7a2978708849d709d
shenmishajing <shenmishajing@gmail.com>
2024-01-07 19:04:40 +0800

    feat(version): add version feat

--- speed_benchmark/__init__.py
+++ speed_benchmark/__init__.py
@@ -1,3 +1,4 @@
+from . import version as version
 from .speed_benchmark import speed_benchmark as speed_benchmark
 from .visualization import draw_line_chart as draw_line_chart
 from .visualization import ncolors as ncolors
